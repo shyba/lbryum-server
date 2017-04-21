b8c260ba20a655de68ff1f198d60bfc49fccb9f9
Jack Robison <jackrobison@lbry.io>
2017-04-21 12:43:36 -0400

    update requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,14 @@ import lbryumserver
 import os
 import sys
 
+requires = [
+    'plyvel==0.9',
+    'jsonrpclib',
+    'python-bitcoinrpc==0.1',
+    'appdirs==1.4.3'
+    'lbryschema==0.0.4',
+]
+
 if sys.platform == "darwin":
     os.environ['CFLAGS'] = "-mmacosx-version-min=10.7 -stdlib=libc++ -I/usr/local/Cellar/leveldb/1.20/include"
     os.environ['LDFLAGS'] = "-L/usr/local/Cellar/leveldb/1.20/lib"
@@ -15,7 +23,7 @@ setup(
     packages=find_packages(base_dir),
     version=lbryumserver.__version__,
     entry_points={'console_scripts': ['lbryum-server = lbryumserver.main:main']},
-    install_requires=['plyvel', 'jsonrpclib', 'python-bitcoinrpc==0.1', 'appdirs'],
+    install_requires=requires,
     description="LBRY Electrum Server",
     author="Thomas Voegtlin",
     author_email="thomasv1@gmx.de",
