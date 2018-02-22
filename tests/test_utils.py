8b9960676265db756a76690a0b9d58495f1ed209
Victor Shyba <victor1984@riseup.net>
2018-02-22 16:26:35 -0300

    fix test_hash_160_to_address using valid lbry addrs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,17 +7,17 @@ from lbryumserver.utils import hash_160_to_address, bc_address_to_hash_160
 class UtilTest(unittest.TestCase):
     def test_hash_160_to_address(self):
         self.assertEqual(hash_160_to_address(None), None)
-        self.assertEqual(hash_160_to_address('04e9fca1'.decode('hex')), None)
-        self.assertEqual(hash_160_to_address('04e9fca1f96e021dfaf35bbea267ec2c60787c1b1337'.decode('hex')), None)
-        self.assertEqual(hash_160_to_address('1ad3b0b711f211655a01142fbb8fecabe8e30b93'.decode('hex')),
-                         '13SrAVFPVW1txSj34B8Bd6hnDbyPsVGa92')
+        self.assertEqual(hash_160_to_address('27e8a203f2'.decode('hex')), None)
+        self.assertEqual(hash_160_to_address('27e8a203f20080826434c8667bb060e80c3ef9fb1337'.decode('hex')), None)
+        self.assertEqual(hash_160_to_address('27e8a203f20080826434c8667bb060e80c3ef9fb'.decode('hex')),
+                         'bGNHjKwxD5pu9ppmLYrVxkR3ahz2C8oHZM')
 
     def test_bc_address_to_hash_160(self):
         self.assertEqual(bc_address_to_hash_160(None), None)
         self.assertEqual(bc_address_to_hash_160(''), None)
-        self.assertEqual(bc_address_to_hash_160('13SrAVFPVW1txSj34B8Bd6hnDbyPsVGa921337'), None)
-        self.assertEqual(bc_address_to_hash_160('13SrAVFPVW1txSj34B8Bd6hnDbyPsVGa92').encode('hex'),
-                         '1ad3b0b711f211655a01142fbb8fecabe8e30b93')
+        self.assertEqual(bc_address_to_hash_160('bGNHjKwxD5pu9ppmLYrVxkR3ahz2C8oHZM1337'), None)
+        self.assertEqual(bc_address_to_hash_160('bGNHjKwxD5pu9ppmLYrVxkR3ahz2C8oHZM').encode('hex'),
+                         '27e8a203f20080826434c8667bb060e80c3ef9fb')
 
 
 if __name__ == '__main__':
